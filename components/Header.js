5a85361a711b954e76bffe2cf693af3d5d1481f5
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-07-02 18:22:07 +0700

    photos

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Search from '../assets/svg/search'
 
 
 const styles = {
@@ -65,8 +66,16 @@ const Header = () => {
           </div>
 
         </nav>
-    </div>
 
+        <div className='flex items-center'>
+          {/* <ConnectButton /> */}
+          <div className={styles.inputContainer}>
+            <Search />
+            <input className={styles.input} placeholder='Search' />
+          </div>
+        </div>
+
+        </div>
     </div>
   )
 }
