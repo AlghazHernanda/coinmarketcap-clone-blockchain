0aaf65ef2c88a8c1d285335a34fa0ff7a2be3006
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-07-04 00:03:59 +0700

    css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { MoralisProvider } from 'react-moralis'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MoralisProvider
+    // udah dimasukin ke env di netlify
     serverUrl='https://vc3dsi3cyghd.usemoralis.com:2053/server'
     appId='V5XCyHfSwEvnwBHo6Px8RBXw4B7BsPdZlsV85MiU'
   >
