105296d082998095015da49931045a16d2bedbfc
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-07-05 00:29:07 +0700

    cmc table

--- components/cmc-table/CMCTable.js
+++ components/cmc-table/CMCTable.js
@@ -28,7 +28,38 @@ const CMCTable = () => {
       }, [getTopTenCoins])
 
   return (
-    <div>CmcTable</div>
+    <div className='text-white font-bold'>
+    <div className='mx-auto max-w-screen-2xl'>
+      <table className='w-full'>
+        {/* <CMCtableHeader /> */}
+
+        {coinData && coinData ? (
+          coinData.map((coin, index) => {
+            return (
+              <CMCtableRow
+                key={index}
+                starNum={coin.cmc_rank}
+                coinName={coin.name}
+                coinSymbol={coin.symbol}
+                coinIcon={btc}
+                showBuy={true}
+                hRate={coin.quote.USD.percent_change_24h}
+                dRate={coin.quote.USD.percent_change_7d}
+                hRateIsIncrement={true}
+                price={coin.quote.USD.price}
+                marketCapValue={coin.quote.USD.market_cap}
+                volumeCryptoValue={coin.quote.USD.volume_24h}
+                volumeValue={coin.total_supply}
+                circulatingSupply={coin.circulating_supply}
+              />
+            )
+          })
+        ) : (
+          <></>
+        )}
+      </table>
+    </div>
+  </div>
   )
 }
 
