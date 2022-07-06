07962c0c51066e060ccec7d8e000d747792fa592
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-07-06 22:27:58 +0700

    update

--- pages/currencies/info.js
+++ pages/currencies/info.js
@@ -21,8 +21,82 @@ const info = () => {
   const [coinSymbol, setCoinSymbol] = useState('')
   const [price, setPrice] = useState('')
 
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const getData = async () => {
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+
+    setCoinName(urlParams.get('coin'))
+    setPrice(Number(urlParams.get('price')).toLocaleString())
+    setCoinSymbol(urlParams.get('symbol'))
+  }
+
   return (
-    <div>info</div>
+    <div className={styles.info}>
+    <Header />
+    <main className={styles.main}>
+      <div className={styles.flexStart}>
+        <div className={styles.tabContainerWrapper}>
+          <div className={styles.flexBetween}>
+            <div className={styles.tabContainer}>
+              <p className={styles.activeTab}>Price</p>
+              <p className={styles.tabItem}>Market Cap</p>
+              <p className={styles.tabItem}>Trading View</p>
+              <p className={styles.tabItem}>History</p>
+            </div>
+
+            <div className={styles.tabContainer}>
+              <p className={styles.activeTab}>1D</p>
+              <p className={styles.tabItem}>2D</p>
+              <p className={styles.tabItem}>1M</p>
+              <p className={styles.tabItem}>3M</p>
+              <p className={styles.tabItem}>1Y</p>
+              <p className={styles.tabItem}>YTD</p>
+              <p className={styles.tabItem}>ALL</p>
+              <p className={styles.tabItem}>LOG</p>
+            </div>
+          </div>
+          <br />
+          {/* <Graph /> */}
+          <br />
+          <div className={styles.flexBetweenCenter}>
+            <div className='flex'>
+              <div className={styles.flexCenter}>
+                <input className='outline-none' type='checkbox' /> &nbsp; USD
+              </div>
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <div className={styles.flexCenter}>
+                <input type='checkbox' /> &nbsp; BTC
+              </div>
+            </div>
+
+            <p>
+              Want more data?{' '}
+              <span className='text-[#6188FF]'>Check out our API</span>
+            </p>
+          </div>
+          <br />
+          <br />
+          {/* <CMCpriceConverter
+            from={coinName}
+            fromSymbol={coinSymbol}
+            fromLogo={solana}
+            toLogo={<Usd />}
+            price={price}
+            to='United States Dollars'
+            toSymbol='USD'
+          /> */}
+        </div>
+
+        <div className='pt-10 ml-5'>
+          {/* <Chat /> */}
+        </div>
+      </div>
+    </main>
+  </div>
   )
 }
 
