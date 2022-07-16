d1c376377fb6f4b047bdf2559bd1ad5754a1c8b2
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-07-16 14:35:22 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,12 @@ import Image from 'next/image'
 import Header from '../components/header'
 import Trending from '../components/Trending'
 import CMCtable from '../components/cmc-table/cmcTable'
+import SwapCryptoModal from '../components/SwapCryptoModal'
 
 const Home: NextPage = () => {
   return (
     <div className='min-h-screen'>
-       <Header />
+      <Header />
       {/* <SwapCryptoModal /> */}
       <div className='mt-10' />
       <Trending />
